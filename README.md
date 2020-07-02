6969aac79f8807dc9278b21ba9acea537d0d2f9f
AndrewCS149 <andrewcs149@gmail.com>
2020-07-01 18:09:14 -0700

    Complete challenge 2. Upload image and create readme.md.

---
+++ README.md
@@ -0,0 +1,21 @@
+This repository contains 4 coding challenges.
+
+---
+
+# 1: Array Max Result 
+
+
+
+# 2: Leap Year Calculator
+
+Given a year, output whether is it a leap year or not.
+
+On the Gregorian calendar, a leap year occurs:
+
+* On every year that is evenly divisible by 4 
+* except every year that is evenly divisible by 100
+* unless the year is also evenly divisible by 400
+
+Do not worry about edge cases.
+
+
--- challenge/challenge/Program.cs
+++ challenge/challenge/Program.cs
@@ -6,9 +6,25 @@ namespace challenge
     {
         static void Main(string[] args)
         {
-            
 
+            Console.WriteLine(isLeapYear(1997));
+            Console.WriteLine(isLeapYear(1996));
+            Console.WriteLine(isLeapYear(1900));
+            Console.WriteLine(isLeapYear(2000));
         }
 
+        public static string isLeapYear(int year)
+        {
+            if (year % 4 == 0)
+            {
+                if (year % 100 == 0 && year % 400 == 0)
+                    return $"{year} is a leap year";
+                else if (year % 100 == 0)
+                    return $"{year} is not a leap year";
+                else
+                    return $"{year} is a leap year";
+            }
+            return $"{year} is not a leap year";
+        }
     }
 }
